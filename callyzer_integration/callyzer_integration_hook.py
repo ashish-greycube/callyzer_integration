1fcce6efc67f19ff8949824e6d99b0fbb16d578e
ashish-greycube <mr.ashish.shah@gmail.com>
2021-09-16 16:31:18 +0530

    fix: check for e.repsone

--- callyzer_integration/callyzer_integration_hook.py
+++ callyzer_integration/callyzer_integration_hook.py
@@ -86,7 +86,10 @@ def fetch_callyzer_data_and_make_integration_request(callyzer_settings):
 	except Exception as e:
 		print(e)
 		frappe.db.set_value('Integration Request', integration_request.name, 'status', 'Failed')
-		frappe.log_error(frappe.get_traceback()+'\n\n\n'+e.response.text, title=_('Callyzer Error'))
+		if e.response:
+			frappe.log_error(frappe.get_traceback()+'\n\n\n'+e.response.text, title=_('Callyzer Error'))
+		else:
+			frappe.log_error(frappe.get_traceback(), title=_('Callyzer Error'))
 		return		
 
 def make_callyzer_call_log_records(call_row,integration_request):
